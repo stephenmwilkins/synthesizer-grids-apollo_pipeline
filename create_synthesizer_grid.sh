b157a752210553ab04ff6f0763b4d9fdc9df3d1f
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-02-21 09:49:51 +0000

    updated

--- create_synthesizer_grid.sh
+++ create_synthesizer_grid.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# synthesizer_dir="/Users/stephenwilkins/Dropbox/Research/data/synthesizer/" # SW's machine
-synthesizer_dir="/research/astrodata/highz/synthesizer/" # apollo
-
-
+grid_dir="/research/astrodata/highz/synthesizer/grids" # apollo
+cloudy_dir="/research/astrodata/highz/synthesizer/cloudy"
 cd ..
 while IFS="" read -r p || [ -n "$p" ]
 do
   arrIN=(${p// / })
   grid=${arrIN[0]}
   echo $grid
-  python3 create_synthesizer_grid.py -grid_name $grid -synthesizer_data_dir $synthesizer_dir -line_calc_method linelist
-done < apollo_production_scripts/grids.txt
+  python3 create_synthesizer_grid.py -grid_name $grid -grid_dir $grid_dir -cloudy_dir $cloudy_dir -line_calc_method linelist
+done < synthesizer-grids-apollo_pipeline/grids.txt
 
 
 
